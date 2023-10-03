6f08738479768acacfdba056edb2a32e073f2766
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-10-03 08:34:03 +0100

    add fastbootd

--- device.mk
+++ device.mk
@@ -6,23 +6,7 @@ LOCAL_PATH := device/samsung/gta8wifi
 PRODUCT_PLATFORM := ums512
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# fastbootd
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl-wrapper.recovery \
-    android.hardware.boot@1.0-impl-wrapper \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.$(PRODUCT_PLATFORM) \
-    bootctrl.$(PRODUCT_PLATFORM).recovery \
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
-# tzdata
-PRODUCT_PACKAGES_ENG += \
-    tzdata_twrp
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
