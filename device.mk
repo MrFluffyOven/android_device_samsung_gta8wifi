760ef3300979a655485e31da77d50ac7221317d1
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-10-03 19:14:43 +0100

    tzdata

--- device.mk
+++ device.mk
@@ -10,3 +10,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+    # Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# tzdata
+PRODUCT_PACKAGES_ENG += \
+    tzdata_twrp
